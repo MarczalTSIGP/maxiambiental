257d8a43c600cde4a40a865ef933b02e3045fc1b
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-20 21:23:08 -0300

    Refactored login style and configured i18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module Maxiambiental
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :pt
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.fallbacks = [:en]
   end
 end
