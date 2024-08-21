b12664f1e71841e761e822f37cea21e98a000024
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-21 20:22:32 -0300

    Refactored folders structure and partials

--- app/controllers/users/devise/sessions_controller.rb
+++ app/controllers/users/devise/sessions_controller.rb
@@ -1,7 +1,7 @@
-class Admin::Devise::SessionsController < Devise::SessionsController
+class Users::Devise::SessionsController < Devise::SessionsController
     prepend_before_action :require_no_authentication
 
-    layout 'layouts/devise/session'
+    layout 'layouts/users/devise/session'
 
     def after_sign_in_path_for(_resource)
         root_path
