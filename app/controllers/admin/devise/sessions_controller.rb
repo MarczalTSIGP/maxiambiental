5e6c490587d69664f206379e2fb6ff507c409723
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-22 19:41:15 -0300

    Added login to admin

--- app/controllers/admin/devise/sessions_controller.rb
+++ app/controllers/admin/devise/sessions_controller.rb
@@ -1,7 +1,7 @@
-class Users::Devise::SessionsController < Devise::SessionsController
+class Admin::Devise::SessionsController < Devise::SessionsController
     prepend_before_action :require_no_authentication
 
-    layout 'layouts/users/devise/session'
+    layout 'layouts/devise/session'
 
     def after_sign_in_path_for(_resource)
         root_path
