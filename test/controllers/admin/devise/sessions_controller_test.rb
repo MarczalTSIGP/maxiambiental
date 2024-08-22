e2c9b642e23ff226ca1d5294df529c3d99d71b73
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-22 20:31:40 -0300

    Refactored test to login

--- test/controllers/admin/devise/sessions_controller_test.rb
+++ test/controllers/admin/devise/sessions_controller_test.rb
@@ -1,25 +1,34 @@
 require "test_helper"
 
 class Admin::Devise::SessionsControllerTest < ActionDispatch::IntegrationTest
+  include ActionView::Helpers::TranslationHelper
 
   setup do
-    @user = FactoryBot.create(:user)
+    @admin = FactoryBot.create(:admin)
   end
 
   test "should get new" do
-    get new_admin_user_session_path
+    get new_admin_session_path
     assert_response :success
   end
 
   test "should login user" do
-    post admin_user_session_path, params: { user: { email: @user.email, password: @user.password } }
+    post admin_session_path, params: { admin: { email: @admin.email, password: @admin.password } }
     assert_redirected_to root_path
-    assert_equal @user.id, session["warden.user.user.key"][0][0]
+    assert_equal t('devise.sessions.signed_in'), flash[:notice]
   end
 
   test "should not login user with invalid credentials" do
-    post admin_user_session_path, params: { user: { email: @user.email, password: "invalid" } }
+    post admin_session_path, params: { admin: { email: @admin.email, password: "invalid" } }
     assert_response :unprocessable_entity
-    assert_nil session["warden.user.user.key"]
+    assert_equal t('devise.failure.invalid', authentication_keys: "Email"), flash[:alert]
+  end
+
+  test "should logout user" do
+    post admin_session_path, params: { admin: { email: @admin.email, password: "invalid" } }
+    delete destroy_admin_session_path
+    assert_redirected_to root_path
+    assert_nil session["warden.user.admin.key"]
+    assert_equal t('devise.sessions.signed_out'), flash[:notice]
   end
 end
