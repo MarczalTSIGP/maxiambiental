f7eef60d43cd9f70274df39db340ba662a6dbee9
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-24 10:48:22 -0300

    Added integration tests to admin login flow

---
+++ test/integration/authentication/login_flow_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+
+class LoginFlowTest < ActionDispatch::IntegrationTest
+  include ActionView::Helpers::TranslationHelper
+
+  setup do
+    @admin = FactoryBot.create(:admin)
+  end
+
+  test "admin login page" do
+    get new_admin_session_path
+    assert_response :success
+
+    assert_select "h1", "Entrar"
+  end
+
+  test "admin login with valid credentials" do
+    get new_admin_session_path
+    assert_response :success
+
+    post admin_session_path, params: { admin: { email: @admin.email, password: 'password' } }
+    assert_redirected_to root_path
+
+    follow_redirect!
+    assert_response :success
+    assert_select "h1", "Maxiambiental, a melhor escolha para sua empresa."
+  end
+
+  test "admin login with invalid credentials" do
+    get new_admin_session_path
+    assert_response :success
+
+    post admin_session_path, params: { admin: { email: @admin.email, password: 'wrong_password' } }
+    assert_response :unprocessable_entity
+
+    assert_select "p", t('devise.failure.invalid', authentication_keys: "Email")
+  end
+
+  test "admin login with empty credentials" do
+    get new_admin_session_path
+    assert_response :success
+
+    post admin_session_path, params: { admin: { email: '', password: '' } }
+    assert_response :unprocessable_entity
+
+    assert_select "p", t('devise.failure.invalid', authentication_keys: "Email")
+  end
+end
