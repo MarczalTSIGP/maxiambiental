f2bfdbd6bd04f845d986587f2d15e56736e35f14
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-25 19:39:55 -0300

    Rubocop fix

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,6 @@
 class UserMailerPreview < ActionMailer::Preview
-    def reset_password_email
-        user = Admin.first
-        Devise::Mailer.reset_password_instructions(user, user.reset_password_token)
-    end
+  def reset_password_email
+    user = Admin.first
+    Devise::Mailer.reset_password_instructions(user, user.reset_password_token)
   end
-  
+end
