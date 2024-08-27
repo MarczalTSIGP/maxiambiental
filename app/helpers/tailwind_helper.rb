5a347b31ad65d91381d4411054b92be0bf8e2fd6
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-27 20:31:06 -0300

    Fixed code with rubocop

--- app/helpers/tailwind_helper.rb
+++ app/helpers/tailwind_helper.rb
@@ -20,12 +20,14 @@ module TailwindHelper
   end
 
   def input_class(additional_classes = '')
-    default_class = 'shadow border border-gray-300 rounded-md w-full p-2.5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-green-700 focus:ring-green-700'
+    default_class = 'shadow border border-gray-300 rounded-md w-full p-2.5 text-gray-700 leading-tight
+                      focus:outline-none focus:shadow-outline focus:border-green-700 focus:ring-green-700'
     "#{default_class} #{additional_classes}"
   end
 
   def button_class(additional_classes = '')
-    default_class = 'w-full text-black py-2 px-4 rounded-md border focus:outline-none focus:shadow-outline inline-flex items-center justify-center'
+    default_class = 'w-full text-black py-2 px-4 rounded-md border focus:outline-none focus:shadow-outline
+                      inline-flex items-center justify-center'
     "#{default_class} #{additional_classes}"
   end
 
