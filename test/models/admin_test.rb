9bccb2986a4d506f397136b484d117317a4a41fd
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-27 20:47:27 -0300

    Fixed by rubocop

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -3,43 +3,48 @@ require 'test_helper'
 class AdminTest < ActiveSupport::TestCase
   def setup
     @admin = Admin.new(
-      email: "admin@example.com",
-      password: "password",
-      name: "Admin Name",
+      email: 'admin@example.com',
+      password: 'password',
+      name: 'Admin Name',
       master: true,
-      bio: "This is a bio.",
-      avatar: "avatar_url",
+      bio: 'This is a bio.',
+      avatar: 'avatar_url',
       active: true
     )
   end
 
-  test "should be valid" do
-    assert @admin.valid?
+  test 'should be valid' do
+    assert_predicate @admin, :valid?
   end
 
-  test "email should be present" do
-    @admin.email = "  "
+  test 'email should be present' do
+    @admin.email = '  '
+
     assert_not @admin.valid?
   end
 
-  test "email should be unique" do
+  test 'email should be unique' do
     duplicate_admin = @admin.dup
     @admin.save
+
     assert_not duplicate_admin.valid?
   end
 
-  test "password should be present" do
-    @admin.password = "  "
+  test 'password should be present' do
+    @admin.password = '  '
+
     assert_not @admin.valid?
   end
 
-  test "master should default to false" do
-    new_admin = Admin.new(email: "new_admin@example.com", password: "password")
+  test 'master should default to false' do
+    new_admin = Admin.new(email: 'new_admin@example.com', password: 'password')
+
     assert_not new_admin.master
   end
 
-  test "active should default to true" do
-    new_admin = Admin.new(email: "new_admin@example.com", password: "password")
+  test 'active should default to true' do
+    new_admin = Admin.new(email: 'new_admin@example.com', password: 'password')
+
     assert new_admin.active
   end
 end
