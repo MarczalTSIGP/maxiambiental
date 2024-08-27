293b90e16d97d96f0429fe1ab43ef67fdbd754b4
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-08-27 20:47:27 -0300

    Fixed by rubocop

--- test/components/alert/alert_component_test.rb
+++ test/components/alert/alert_component_test.rb
@@ -4,23 +4,23 @@ require 'test_helper'
 
 class Alert::AlertComponentTest < ViewComponent::TestCase
   def setup
-    @message = "This is a message to test the view!"
+    @message = 'This is a message to test the view!'
   end
 
-  test "renders alert with correct message" do
+  test 'renders alert with correct message' do
     render_inline(Alert::AlertComponent.new(
-      type: :error,
-      message: @message,
-    ))
+                    type: :error,
+                    message: @message
+                  ))
 
     assert_text @message
   end
 
-  test "close button is present and configured correctly" do
+  test 'close button is present and configured correctly' do
     render_inline(Alert::AlertComponent.new(
-      type: :success,
-      message: @message,
-    ))
+                    type: :success,
+                    message: @message
+                  ))
 
     assert_selector "button[data-action='click->alert#close']", count: 1
   end
