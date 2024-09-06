6b004183e1be429ebbdfa92ea5e772673130458f
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-09-05 21:21:54 -0300

    Fixed sidebar toggle

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -4,9 +4,21 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["sidebar"];
 
+  connect() {
+    document.addEventListener("click", this.closeOnOutsideClick.bind(this));
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.closeOnOutsideClick.bind(this));
+  }
+
   toggle() {
-    console.log("test");
-    this.sidebarTarget.classList.toggle("translate-x-0");
     this.sidebarTarget.classList.toggle("-translate-x-full");
+  }
+
+  closeOnOutsideClick(event) {
+    if (!this.sidebarTarget.contains(event.target) && !event.target.closest("[data-action='click->sidebar#toggle']")) {
+      this.sidebarTarget.classList.add("-translate-x-full");
+    }
   }
 }
