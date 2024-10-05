c75dfb984c3401b35903efff7a2cefc5e8b8b98a
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-10-05 19:07:44 -0300

    Added header component test

--- test/components/home/header/header_component_test.rb
+++ test/components/home/header/header_component_test.rb
@@ -3,10 +3,37 @@
 require "test_helper"
 
 class Home::Header::HeaderComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(Home::Header::HeaderComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_should_renders_a_navbar
+    render_inline(Home::Header::HeaderComponent.new)
+    assert_selector("nav")
+  end
+
+  def test_should_renders_a_navbar_with_a_list_of_links
+    render_inline(Home::Header::HeaderComponent.new)
+
+    assert_selector('ul')
+
+    assert_selector('li', text: "Cursos")
+    assert_selector('li', text: "Galeria de Fotos")
+    assert_selector('li', text: "Sobre Nós")
+    assert_selector('li', text: "Fale Conosco")
+  end
+
+  def test_should_renders_a_navbar_with_link_to_login
+    render_inline(Home::Header::HeaderComponent.new)
+
+    assert_selector('a', text: "Entrar")
+  end
+
+  def test_should_renders_a_navbar_with_link_to_register
+    render_inline(Home::Header::HeaderComponent.new)
+
+    assert_selector('a', text: "Cadastrar")
+  end
+
+  def test_should_renders_a_sidebar
+    render_inline(Home::Header::HeaderComponent.new)
+
+    assert_selector("aside")
   end
 end
