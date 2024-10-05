e29a1412ac23a53357ae94c72f9efc83e32d3117
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-10-05 19:31:56 -0300

    Added devise clients

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,4 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :trackable,
          :rememberable, :validatable, :lockable, :timeoutable
 
