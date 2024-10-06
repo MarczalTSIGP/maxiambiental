f263c2419a555fc4e9edd65166fb2e6d2703482e
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-10-06 14:22:50 -0300

    Customized confirmation emial

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,9 @@ class UserMailerPreview < ActionMailer::Preview
     user = Admin.first
     Devise::Mailer.reset_password_instructions(user, user.reset_password_token)
   end
+
+  def confirmation_instructions
+    user = Client.first
+    Devise::Mailer.confirmation_instructions(user, user.confirmation_token)
+  end
 end
