63d72c536f4be97b6acba3a65760c29eb414a445
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-10-16 21:55:43 -0300

    Added syustem tests

--- test/system/profile/admin_profile_test.rb
+++ test/system/profile/admin_profile_test.rb
@@ -6,49 +6,19 @@ class AdminProfileTest < ApplicationSystemTestCase
   #   sign_in @admin
   # end
 
-  # test 'should display the edit profile page correctly' do
-  #   visit edit_admin_profile_path(@admin)
+  # test "Admin can update profile" do
+  #   visit admin_edit_profile_path
 
-  #   assert_selector 'h1', text: t('profile.edit_title')
-  #   assert_text t('profile.edit_description')
-
-  #   within('div', text: t('profile.edit_profile')) do
-  #     assert_text t('profile.edit_profile_description')
-  #     assert_selector "input[name='user[name]']"
-  #     assert_selector "input[name='user[email]']"
+  #   within(".grid") do
+  #     fill_in 'admin_name', with: "New name"
+  #     fill_in 'admin_email', with: "newemail@example.com"
+  #     fill_in 'admin_current_password', with: @admin.password
+      
+  #     click_on t('buttons.save')
   #   end
 
-  #   assert_selector "input[type='file']"
-
-  #   within('div', text: t('profile.edit_password')) do
-  #     assert_text t('profile.edit_password_description')
-  #     assert_selector "input[name='user[current_password]']"
-  #     assert_selector "input[name='user[password]']"
-  #     assert_selector "input[name='user[password_confirmation]']"
-  #   end
-  # end
-
-  # test 'should allow the admin to update personal information' do
-  #   visit edit_admin_profile_path(@admin)
-
-  #   fill_in :admin_name, with: 'Novo Nome'
-  #   fill_in :admin_email, with: 'novoemail@example.com'
-  #   click_button t('buttons.save')
-
-  #   assert_text t('flash_messages.profile_updated')
-  #   @admin.reload
-  #   assert_equal 'Novo Nome', @admin.name
-  #   assert_equal 'novoemail@example.com', @admin.email
-  # end
-
-  # test 'should allow the admin to change the password' do
-  #   visit edit_admin_profile_path(@admin)
-
-  #   fill_in t('activerecord.attributes.admin.current_password'), with: 'senha_antiga'
-  #   fill_in t('activerecord.attributes.admin.new_password'), with: 'nova_senha'
-  #   fill_in t('activerecord.attributes.admin.new_password_confirmation'), with: 'nova_senha'
-  #   click_button t('buttons.change_password')
-
-  #   assert_text t('flash_messages.profile_updated')
+  #   assert_text "Profile was successfully updated"
+  #   assert_equal "New name", @admin.reload.name
+  #   assert_equal "newemail@example.com", @admin.reload.email
   # end
 end
