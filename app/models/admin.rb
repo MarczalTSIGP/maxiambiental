d414219df99fe4b15a54601217ada6e6d7e0aa42
Diego Marczal <dmarczal@gmail.com>
2024-10-18 05:39:57 -0300

    Fixed default avatar path to genarete correct file name

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -12,7 +12,7 @@ class Admin < ApplicationRecord
   has_one_attached :avatar
 
   def avatar_url
-    return '/assets/default-avatar.png' unless avatar.attached?
+    return ActionController::Base.helpers.asset_path('default-avatar.png') unless avatar.attached?
 
     Rails.application.routes.url_helpers.rails_blob_url(avatar, only_path: true)
   end
