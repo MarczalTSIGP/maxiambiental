c0cb33e7037467e575c1994e686b27293b36ac37
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-10-19 11:31:34 -0300

    Added layout to mails

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 #   end
 
 Admin.create_with(password: '123456', name: 'Maxiambiental').find_or_create_by!(email: 'admin@maxiambiental.com')
+
+Client.create_with(password: '123456').find_or_create_by!(email: 'client@maxiambiental.com')
