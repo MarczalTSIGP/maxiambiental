2feb68c4216eafd29081b11d8db24df78c626d59
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2024-10-20 11:12:23 -0300

    Added controller test to login

--- app/controllers/clients/devise/sessions_controller.rb
+++ app/controllers/clients/devise/sessions_controller.rb
@@ -3,6 +3,10 @@
 class Clients::Devise::SessionsController < Devise::SessionsController
   prepend_before_action :require_no_authentication, only: [:new, :create]
 
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
   def after_sign_out_path_for(_resource)
     new_client_session_path
   end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @client = Client.new(email: 'new_client@example.com', password: 'password')
+  end
+
+  context 'validations' do
+    should validate_presence_of(:email)
+    should validate_presence_of(:password)
+  end
+
+  test 'client active attribute defaults to true' do
+    assert @client.active
+  end
 end
