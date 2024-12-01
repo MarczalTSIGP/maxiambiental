d5f9c581c52eb20cb2a775fd93c5b02e39aaceac
Jacson Souza <jacsonsavio7@gmail.com>
2024-12-01 15:46:57 -0300

    Added name in client registration

--- app/controllers/clients/devise/registrations_controller.rb
+++ app/controllers/clients/devise/registrations_controller.rb
@@ -2,10 +2,15 @@
 
 class Clients::Devise::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :require_no_authentication, only: [:new, :create]
+  before_action :configure_sign_up_params, only: [:create]
 
   layout 'layouts/clients/devise/sessions'
 
   def after_inactive_sign_up_path_for(_resource)
     new_client_session_path
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
