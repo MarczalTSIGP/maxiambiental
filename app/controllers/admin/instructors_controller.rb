8bb3e7908ab40354d98cf0f65e87766ca32c6cd7
Jacson Souza <jacsonsavio7@gmail.com>
2024-12-04 19:37:43 -0300

    add controller to create instructors

--- app/controllers/admin/instructors_controller.rb
+++ app/controllers/admin/instructors_controller.rb
@@ -1,6 +1,27 @@
 class Admin::InstructorsController < ApplicationController
   layout 'admin/application'
+
+  def index
+    @instructors = Instructor.all
+  end
+
   def new
     @instructor = Instructor.new
+  end
+
+  def create
+    @instructor = Instructor.new(instructor_params)
+
+    if @instructor.save
+      redirect_to admin_instructors_path, notice: 'Instructor was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def instructor_params
+    params.require(:instructor).permit(:name, :email, :phone, :resume)
   end
 end
