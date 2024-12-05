c3c63150c073446b507014fe62aa93393a24c6c4
Jacson Souza <jacsonsavio7@gmail.com>
2024-12-05 13:00:50 -0300

    Added instructors validation

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,2 +1,14 @@
 class Instructor < ApplicationRecord
+  validates :email,
+            presence: true,
+            format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/ }
+
+  validates :phone,
+            presence: true,
+            format: { with: /\A\(?\d{2}\)?\s?\d{4,5}-?\d{4}\z/ }
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :resume, presence: true
+
+  has_one_attached :avatar
 end
