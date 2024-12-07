819dafd8e2d033bc8e235e170a8a46f911565062
Jacson Souza <jacsonsavio7@gmail.com>
2024-12-07 11:54:09 -0300

    Added active column, not null values and index to columns name and email in migration

--- db/migrate/20241203222139_create_instructors.rb
+++ db/migrate/20241203222139_create_instructors.rb
@@ -1,12 +1,16 @@
 class CreateInstructors < ActiveRecord::Migration[7.1]
   def change
     create_table :instructors do |t|
-      t.string :name
-      t.string :email
-      t.string :phone
-      t.text :resume
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :phone, null: false
+      t.boolean :active, default: true, null: false
+      t.text :resume, null: false
 
       t.timestamps
     end
+
+    add_index :instructors, :email, unique: true
+    add_index :instructors, :name
   end
 end
