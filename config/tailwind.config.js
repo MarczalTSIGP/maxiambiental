2ff02bdbb0508f12e8cacc3884d408188f970d01
Diego Marczal <dmarczal@gmail.com>
2025-03-05 11:27:42 -0300

    Fixed missing location in the config Tailwind file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/views/**/*.{erb,haml,html,slim}",
+    "./app/inputs/**/*.rb",
     "./app/components/**/*",
     "./config/initializers/*.rb",
   ],
