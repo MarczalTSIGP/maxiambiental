062fd342fe735fef9ebdf2c23a708df76b61c0a8
Jacson Souza <jacsonsavio7@gmail.com>
2025-03-25 17:20:40 -0300

    Refactor input image with preview.

--- app/inputs/image_input.rb
+++ app/inputs/image_input.rb
@@ -1,69 +1,128 @@
+# app/inputs/image_input.rb
 class ImageInput < SimpleForm::Inputs::Base
-  def input(wrapper_options = nil)
-    @merged_input_options = build_merged_input_options(wrapper_options)
+  DEFAULT_IMAGE_PATH = 'card-default.png'.freeze
+  CONTROLLER_NAME = 'input-image'.freeze
+  ACCEPTED_TYPES = 'image/png, image/jpeg'.freeze
 
+  def input(wrapper_options = nil)
     template.content_tag(:div, wrapper_attributes) do
-      preview_image_tag + input_components
+      preview_container + default_upload_area(merged_options(wrapper_options))
     end
   end
 
   private
 
-  def build_merged_input_options(wrapper_options)
-    merged = merge_wrapper_options(input_html_options, wrapper_options)
-    merged[:class] = [merged[:class], 'peer sr-only'].compact.join(' ')
-    merged[:'data-action'] = 'change->input-image#loadFile'
-    merged
+  module HtmlBuilders
+    def icon_with_text(icon_class, text)
+      template.content_tag(:div, class: 'text-center px-4') do
+        template.icon('fa-solid', icon_class, class: 'fa-3x mb-6 text-gray-400') +
+          template.content_tag(:p, text, class: 'mb-3 text-sm text-gray-500 font-semibold')
+      end
+    end
+
+    def wrapped_container(attributes, &)
+      template.content_tag(:div, attributes, &)
+    end
+  end
+
+  include HtmlBuilders
+
+  def merged_options(wrapper_options)
+    merge_wrapper_options(input_html_options, wrapper_options).deep_merge(
+      class: 'hidden',
+      accept: ACCEPTED_TYPES,
+      data: {
+        "#{CONTROLLER_NAME}-target": 'input',
+        action: 'change->input-image#loadFile'
+      }
+    )
   end
 
   def wrapper_attributes
     {
-      data: { controller: 'input-image' },
-      class: 'flex items-center gap-6'
+      class: 'relative w-full h-64 border-dashed border-2 border-gray-300 rounded-lg bg-white',
+      data: { controller: CONTROLLER_NAME }
     }
   end
 
-  def input_components
-    template.content_tag(:div, class: 'flex items-center sm:flex-col gap-4') do
-      input_label
+  def preview_container
+    wrapped_container(preview_attributes) do
+      wrapped_container(center_container_attributes) do
+        wrapped_container(aspect_ratio_container_attributes) do
+          preview_image + remove_button
+        end
+      end
     end
   end
 
-  def input_label
-    template.content_tag(:label, class: 'cursor-pointer block') do
-      select_button + @builder.file_field(attribute_name, @merged_input_options)
+  def preview_image
+    template.image_tag(
+      preview_image_url,
+      data: { "#{CONTROLLER_NAME}-target": 'previewImage' },
+      class: 'w-full h-full object-cover rounded-xl shadow-lg'
+    )
+  end
+
+  def preview_attributes
+    {
+      class: class_names('w-full h-full', hidden: !image_attached?),
+      data: {
+        "#{CONTROLLER_NAME}-target": 'previewContainer',
+        'existing-image': image_attached?
+      }
+    }
+  end
+
+  def default_upload_area(options)
+    template.content_tag(:label, label_attributes) do
+      upload_content + @builder.file_field(attribute_name, options)
     end
   end
 
-  def select_button
-    template.content_tag(:span, 'Escolher imagem', class: 'block btn-outline',
-                                                   data: { 'input-image-target' => 'labelText' })
+  def label_attributes
+    {
+      class: class_names(
+        'absolute inset-0 flex flex-col items-center justify-center rounded-lg cursor-pointer hover:bg-gray-100',
+        'hidden' => image_attached?
+      ),
+      data: { "#{CONTROLLER_NAME}-target": 'defaultContent' }
+    }
   end
 
-  def preview_image_tag
-    template.content_tag(:div, class: 'shrink-0') do
-      template.image_tag(
-        preview_image_url,
-        id: 'preview_img',
-        data: { 'input-image-target' => 'preview' },
-        class: 'h-16 w-16 object-cover aspect-square rounded-md border-solid border-black',
-        alt: object.try(:name).presence
-      )
-    end
+  def upload_content
+    icon_with_text('cloud-arrow-up', 'Clique para enviar')
+  end
+
+  def remove_button
+    template.button_tag(
+      'X',
+      type: 'button',
+      class: 'absolute top-2 right-2 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center',
+      data: { action: "#{CONTROLLER_NAME}#removeImage" }
+    )
   end
 
   def preview_image_url
-    if object.send(attribute_name).attached?
-      Rails.application.routes.url_helpers.rails_blob_url(
-        object.send(attribute_name).attachment,
-        only_path: true
-      )
-    else
-      default_image_url
-    end
+    image_attached? ? attached_image_url : DEFAULT_IMAGE_PATH
   end
 
-  def default_image_url
-    ActionController::Base.helpers.asset_path('card-default.png')
+  def attached_image_url
+    Rails.application.routes.url_helpers.rails_blob_url(
+      object.send(attribute_name).blob,
+      only_path: true
+    )
+  end
+
+  def image_attached?
+    @image_attached ||= object.send(attribute_name).attached?
+  end
+
+  # Helper methods para atributos de containers
+  def center_container_attributes
+    { class: 'w-full h-full p-6 flex items-center justify-center' }
+  end
+
+  def aspect_ratio_container_attributes
+    { class: 'relative h-full aspect-[3/4]' }
   end
 end
--- app/javascript/controllers/input_image_controller.js
+++ app/javascript/controllers/input_image_controller.js
@@ -1,19 +1,81 @@
 import { Controller } from '@hotwired/stimulus';
 
-// Connects to data-controller="input-image"
 export default class extends Controller {
-    static targets = ['preview'];
+    static targets = [
+        'input',
+        'previewContainer',
+        'defaultContent',
+        'previewImage',
+        'removeButton',
+    ];
 
-    loadFile(event) {
-        const input = event.target;
-        if (input.files && input.files[0]) {
-            const file = input.files[0];
+    connect() {
+        this.initializeExistingPreview();
+        this.inputTarget.addEventListener('change', this.displayPreview.bind(this));
+    }
 
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                this.previewTarget.src = e.target.result;
-            };
-            reader.readAsDataURL(file);
+    disconnect() {
+        this.inputTarget.removeEventListener(
+            'change',
+            this.displayPreview.bind(this),
+        );
+    }
+
+    initializeExistingPreview() {
+        if (this.hasExistingImage) {
+            this.showPreviewContainer();
         }
+    }
+
+    displayPreview(event) {
+        const [file] = event.target.files;
+
+        if (!file) return;
+
+        this.readAndDisplayImage(file);
+    }
+
+    removeImage(event) {
+        event.preventDefault();
+        this.resetFileInput();
+        this.hidePreviewContainer();
+    }
+
+    // Private methods
+    readAndDisplayImage(file) {
+        const reader = new FileReader();
+
+        reader.onload = (e) => this.updatePreview(e.target.result);
+        reader.onerror = () => this.handleImageError();
+        reader.readAsDataURL(file);
+    }
+
+    updatePreview(imageSource) {
+        this.previewImageTarget.src = imageSource;
+        this.showPreviewContainer();
+    }
+
+    showPreviewContainer() {
+        this.previewContainerTarget.classList.remove('hidden');
+        this.defaultContentTarget.classList.add('hidden');
+    }
+
+    hidePreviewContainer() {
+        this.previewContainerTarget.classList.add('hidden');
+        this.defaultContentTarget.classList.remove('hidden');
+        this.previewImageTarget.src = '';
+    }
+
+    resetFileInput() {
+        this.inputTarget.value = '';
+    }
+
+    get hasExistingImage() {
+        return this.previewContainerTarget.dataset.existingImage === 'true';
+    }
+
+    handleImageError() {
+        console.error('Error loading image preview');
+        this.hidePreviewContainer();
     }
 }
