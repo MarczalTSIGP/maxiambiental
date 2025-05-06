c09630c9529dcfbe49fbd12e5d0c35c6db8a7f05
Jacson Souza <jacsonsavio7@gmail.com>
2025-05-06 13:53:36 -0300

    Added test for full title helper.

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'full title without param' do
+    assert_equal I18n.t('app_name'), full_title
+  end
+
+  test 'full title with param' do
+    assert_equal "Home | #{I18n.t('app_name')}", full_title('Home')
+  end
+
+  test 'full title with nil param' do
+    assert_equal I18n.t('app_name'), full_title(nil)
+  end
+end
