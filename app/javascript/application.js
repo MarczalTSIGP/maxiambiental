5fedaff87f0f1dbc5474e843aab0288500d40d8c
Jacson Souza <111143365+jacsonsouza@users.noreply.github.com>
2025-05-06 14:08:54 -0300

    @jacsonsouza/feature/pwa (#24)
    * Added pwa config.
    
    * Removed comments
    
    * Refactored land app name
    
    ---------
    
    Co-authored-by: Prof. Diego Marczal <37582258+MarczalTSIGP@users.noreply.github.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import '@hotwired/turbo-rails'
-import 'controllers'
-import 'preserve_scroll'
+import '@hotwired/turbo-rails';
+import 'controllers';
+import 'preserve_scroll';
+import 'pwa_service_worker';
 
-import 'trix'
-import '@rails/actiontext'
+import 'trix';
+import '@rails/actiontext';
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin 'preserve_scroll', to: 'lib/preserve_scroll.js'
+pin 'pwa_service_worker', to: 'lib/pwa_service_worker.js'
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'trix'
