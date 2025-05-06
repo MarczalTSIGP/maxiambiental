8aa6ca5d1734e2a03223206607e7fc61a26d9ee1
Jacson Souza <jacsonsavio7@gmail.com>
2025-05-06 19:47:47 -0300

    Added controller tests.

--- test/controllers/admin/clients_controller_test.rb
+++ test/controllers/admin/clients_controller_test.rb
@@ -6,19 +6,106 @@ class Admin::ClientsControllerTest < ActionDispatch::IntegrationTest
     sign_in @admin
   end
 
-  test 'should get index' do
-    get admin_clients_url
+  context 'GET #index' do
+    should 'return success' do
+      get admin_clients_path
 
-    assert_response :success
+      assert_response :success
+    end
+
+    should 'list all clients' do
+      clients = FactoryBot.create_list(:client, 3)
+      get admin_clients_path
+
+      clients.each { |client| assert_includes response.body, client.name }
+    end
   end
 
-  test 'should get all clients' do
-    clients = FactoryBot.create_list(:client, 3)
+  context 'GET #new' do
+    should 'return success' do
+      get new_admin_client_path
 
-    get admin_clients_url
-
-    clients.each do |client|
-      assert_select 'tr', text: client.name
+      assert_response :success
     end
+  end
+
+  context 'GET #edit' do
+    should 'return success' do
+      get edit_admin_client_path(FactoryBot.create(:client))
+
+      assert_response :success
+    end
+  end
+
+  context 'POST #create' do
+    should 'create clients with valid parameters' do
+      assert_difference('Client.count') do
+        post admin_clients_path, params: { client: valid_client_params }
+      end
+
+      assert_redirected_to admin_clients_path
+      assert_flash_created(Client)
+      assert_not_nil Client.last.encrypted_password
+    end
+
+    should 'not create client with invalid parameters' do
+      assert_no_difference('Client.count') do
+        post admin_clients_path, params: { client: invalid_client_params }
+      end
+      assert_response :unprocessable_entity
+    end
+  end
+
+  context 'PATCH #update' do
+    should 'update client with valid parameters' do
+      client = FactoryBot.create(:client)
+      patch admin_client_path(client), params: { client: { name: 'Updated Name' } }
+
+      assert_redirected_to admin_clients_path
+      assert_flash_updated(Client)
+      assert_equal 'Updated Name', client.reload.name
+    end
+
+    should 'not update client with invalid parameters' do
+      client = FactoryBot.create(:client)
+      original_name = client.name
+
+      patch admin_client_path(client), params: { client: { name: '' } }
+
+      assert_response :unprocessable_entity
+      assert_equal original_name, client.reload.name
+    end
+  end
+
+  context 'DELETE #destroy' do
+    should 'destroy client' do
+      client = FactoryBot.create(:client)
+      assert_difference('Client.count', -1) { delete admin_client_path(client) }
+
+      assert_redirected_to admin_clients_path
+      assert_flash_destroyed(Client)
+    end
+  end
+
+  private
+
+  def valid_client_params
+    { name: 'New Client', email: 'new@client.com', active: true }
+  end
+
+  def invalid_client_params
+    { name: '', email: '' }
+  end
+
+  def assert_flash_created(model)
+    assert_equal flash[:notice], I18n.t('flash_messages.created', model: model.model_name.human)
+  end
+
+  def assert_flash_updated(model)
+    assert_equal flash[:notice], I18n.t('flash_messages.updated', model: model.model_name.human)
+  end
+
+  def assert_flash_destroyed(model)
+    assert_equal flash[:notice], I18n.t('flash_messages.destroyed', model: model.model_name.human)
   end
 end
