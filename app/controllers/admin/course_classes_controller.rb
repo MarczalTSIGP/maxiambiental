7c9196ff3be2c8ce9339b399c8263d8c8da4785f
Jacson Souza <jacsonsavio7@gmail.com>
2025-05-13 20:48:40 -0300

    Added edit classes page.

--- app/controllers/admin/course_classes_controller.rb
+++ app/controllers/admin/course_classes_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CourseClassesController < Admin::BaseController
+  before_action :set_course_class, only: [:show, :edit, :update]
+
   def index
     @course_classes = CourseClass.includes([:course, :instructor])
                                  .order(:start_at)
@@ -6,9 +8,13 @@ class Admin::CourseClassesController < Admin::BaseController
                                  .page(params[:page])
   end
 
+  def show; end
+
   def new
     @course_class = CourseClass.new
   end
+
+  def edit; end
 
   def create
     @course_class = CourseClass.new(course_class_params)
@@ -21,7 +27,20 @@ class Admin::CourseClassesController < Admin::BaseController
     end
   end
 
+  def update
+    if @course_class.update(course_class_params)
+      redirect_to admin_course_classes_path,
+                  notice: t('flash_messages.updated', model: CourseClass.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_course_class
+    @course_class = CourseClass.find(params[:id])
+  end
 
   def course_class_params
     params.expect(course_class: [:name, :address, :city, :period, :about, :programming, :start_at, :end_at, :course_id,
--- app/models/course_class.rb
+++ app/models/course_class.rb
@@ -14,7 +14,7 @@ class CourseClass < ApplicationRecord
   belongs_to :course
   belongs_to :instructor
 
-  validates :name, :start_at, :end_at, :period, :address, :city, presence: true
+  validates :name, :start_at, :end_at, :period, :address, :city, :about, :programming, presence: true
 
   validates :end_at, comparison: { greater_than: :start_at }
 
