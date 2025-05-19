5c1b26adedd6a52f9322326cb984137448bdd3af
Jacson Souza <jacsonsavio7@gmail.com>
2025-05-19 14:06:02 -0300

    Added system tests.

--- test/factories/course_classes.rb
+++ test/factories/course_classes.rb
@@ -3,15 +3,15 @@ FactoryBot.define do
     sequence(:name) { |n| "Turma #{n}-#{Faker::Lorem.characters(number: 2).upcase}" }
     start_at { 1.week.from_now.beginning_of_day + 9.hours }
     end_at { start_at + 2.months }
-    period { '08:00 - 10:00' }
+    schedule { '08:00 - 10:00' }
     active { true }
     address { Faker::Address.street_address }
-    city { Faker::Address.city }
 
     course
     instructor
 
     about { Faker::Lorem.paragraph(sentence_count: 3) }
     programming { Faker::Lorem.paragraph(sentence_count: 3) }
+    payments_info { Faker::Lorem.paragraph(sentence_count: 3) }
   end
 end
