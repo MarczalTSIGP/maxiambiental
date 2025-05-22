f4db568b5bfc2090bdc5b52adc23c4d25aac0ae9
Jacson Souza <jacsonsavio7@gmail.com>
2025-05-22 16:20:51 -0300

    Added course class public pages.

--- app/controllers/course_classes_controller.rb
+++ app/controllers/course_classes_controller.rb
@@ -1,5 +1,21 @@
 class CourseClassesController < ApplicationController
+  before_action :set_course_class, except: [:index]
+
   def index
     @course_classes = CourseClass.includes([:course]).order(:start_at)
+  end
+
+  def show; end
+
+  def programming; end
+
+  def instructor; end
+
+  def terms; end
+
+  private
+
+  def set_course_class
+    @course_class = CourseClass.find(params[:id])
   end
 end
